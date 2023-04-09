f31e9c786ffb9d1c8f4157a8465af54cbefefc7f
ManshiMishra <manshimishra2507@gmail.com>
2023-04-10 00:33:50 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] arg){
+        RWOnly obj=new RWOnly();
+
+        obj.setName("Arsalan");
+        String ans= obj.getName();
+
+        System.out.println(ans);
+    }
 }
